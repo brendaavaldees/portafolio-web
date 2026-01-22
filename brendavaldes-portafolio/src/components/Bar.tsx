a4c3cd74596518b3a72bdb6fc1836c92f3a5f9eb
Brenda Valdes <brenda.leticia.vp@gmail.com>
2026-01-21 21:21:06 -0700

    Add initial structure

---
+++ brendavaldes-portafolio/src/components/Bar.tsx
@@ -0,0 +1,15 @@
+
+
+export default function MainHeader() {
+  return (
+    <main className="bg-amber-50">
+     <ul className="grid grid-cols-4 p-1.5">
+      <li className="">
+        <img src="../assets/icons/keroppi-icon.gif" alt="frog" />
+      </li>
+      <li className="font-extralight text-sm col-span-2 text-purple-complement">૮꒰ ˶• ༝ •˶꒱ა ♡ by brenda valdes</li>
+      <li className="col-end-7"><img src="../assets/icons/start-exit-icon.png" alt="" /></li>
+     </ul>
+    </main>
+  );
+}
---
+++ brendavaldes-portafolio/src/components/MainCard.tsx
@@ -0,0 +1,9 @@
+import MainHeader from "./Bar";
+
+export default function MainCard() {
+  return (
+    <div className=" w-[90%] max-w-2xl bg-amber-50 border border-dashed border-purple-complement rounded-lg p-2 text-center font-title text-title">
+      <MainHeader />
+    </div>
+  );
+}
